a114583bafd00f179d1fc06b4f89a3042ab7c045
Chutipong Roobklom <khunemz@gmail.com>
2020-05-04 21:28:35 +0700

    feat/routing and service

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,20 +1,32 @@
 import Component from '@glimmer/component';
-import { action} from '@ember/object';
-
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import AuthService from 'shlack/services/auth';
 export default class LoginFormComponent extends Component {
-    
-    loginAsUserWithId = (val) => {
-        console.log(val);
-    }
-    /**
-     * 
-     * @param {Event and target: HTMLFormElement} evt 
-     */
-    @action
-    onLoginFormSubmit(evt) {
-        evt.preventDefault();
-        const { target } = evt;
-        const val = target.querySelector('select').value;
-        this.loginAsUserWithId(val);
-    }
+  // Chutipong : Tracked is like state changed
+  @tracked
+  userId = null;
+
+  /**
+   * @type { AuthService}
+   */
+  @service auth;
+
+  get isDisabled() {
+    return !this.userId;
+  }
+
+  @action
+  onSelectChanged(evt) {
+    this.userId = evt.target.value;
+  }
+
+  @action
+  onLoginFormSubmit(evt) {
+    evt.preventDefault();
+    const { target } = evt;
+    const val = target.querySelector('select').value;
+    this.auth.loginWithUserId(val);
+  }
 }
